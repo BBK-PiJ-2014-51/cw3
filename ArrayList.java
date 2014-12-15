1715bf8b219b39fae445c12d9e91717d4ae1a617
Caleb <caleb@whataloadofrubbish.com>
2014-12-15 20:41:24 +0000

    init commit for ArrayList, added attributes and empty methods

---
+++ ArrayList.java
@@ -0,0 +1,44 @@
+
+public class ArrayList implements List {
+	private int size = 0;
+	private int capacity = 10;
+	private Object[] items = new Object[capacity];
+	
+	
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
