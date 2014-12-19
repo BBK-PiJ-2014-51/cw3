920a7fe5ba07359a94402a6bc017991ef82bde18
Caleb <cclayt02@dcs.bbk.ac.uk>
2014-12-19 12:58:18 -0600

    init commit, node implementation

---
+++ NodeImpl.java
@@ -0,0 +1,37 @@
+
+public class NodeImpl implements Node{
+	private Node next;
+	private Object value;
+	
+	public NodeImpl(){
+		next = null;
+		value = null;
+	}
+	
+	public NodeImpl(Object value){
+		next = null;
+		this.value = value;
+	}
+	
+	@Override
+	public Node getNext() {
+		return next;
+	}
+
+	@Override
+	public boolean setNext(Node next) {
+		this.next = next;
+		return true;
+	}
+
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
+	public boolean setValue(Object value) {
+		this.value = value;
+		return true;
+	}
+}
