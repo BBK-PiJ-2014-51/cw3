bc97d498071095d04ebd26d4a017481ba7d48c73
Caleb <cclayt02@dcs.bbk.ac.uk>
2014-12-19 12:50:45 -0600

    node interface init commit

---
+++ Node.java
@@ -0,0 +1,29 @@
+public interface Node {
+	
+	/**
+	 * Gets the next element in the list
+	 * 
+	 * @return next element in the list
+	 */
+	Object getNext();
+	
+	/**
+	 * Sets the next element in the list
+	 * @param element to be added to the list
+	 * @return true if added to the list, false otherwise
+	 */
+	boolean setNext(Node next);
+	
+	/**
+	 * Gets object stored in node of list
+	 * @return object stored in node
+	 */
+	Object getValue();
+	
+	/**
+	 * Stores object in node
+	 * @param object to be stored in the node
+	 * @return false if object is stored, false otherwise
+	 */
+	boolean setValue(Object value);
+}
