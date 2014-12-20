b2f6e1c771a62a55aeaba64b2199545f472520c6
Caleb <caleb@whataloadofrubbish.com>
2014-12-20 10:04:00 -0600

    init commit for functional list classes, empty methods only

---
+++ FunctionalArrayList.java
@@ -0,0 +1,15 @@
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+	@Override
+	public ReturnObject head() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FunctionalList rest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ FunctionalLinkedList.java
@@ -0,0 +1,15 @@
+
+public class FunctionalLinkedList extends LinkedList implements FunctionalList {
+	@Override
+	public ReturnObject head() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FunctionalList rest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
