89480e3c764a129dedc1e56846ee59b0fd55f406
Caleb <cclayt02@dcs.bbk.ac.uk>
2014-12-20 10:47:02 -0600

    moved shared methods to super class

--- Test.java
+++ Test.java
@@ -34,4 +34,33 @@ public abstract class Test {
 			System.out.println(String.valueOf(list.get(i).getReturnValue()));
 		}
 	}
+	
+	protected void testForEqualLists(List listA, List listB, boolean expectedResult, String testId) {
+		System.out.print(testId + ": ");
+		boolean areEqual = true;
+		if (listA.size() != listB.size()) areEqual = false;
+		if (areEqual){
+			for (int i = 0; i < listA.size(); i++){
+				if (!listA.get(i).getReturnValue().equals(listB.get(i).getReturnValue())) areEqual = false;
+			}
+		}
+		if (expectedResult){
+			if (areEqual) System.out.println("passed.");
+			else System.out.println("FAILED. Lists are not equal.");
+		} else {
+			if (!areEqual) System.out.println("passed.");
+			else System.out.println("FAILED. Lists are equal.");
+		}
+	}
+	
+	protected void testReference(Object a, Object b, boolean expectedResult, String testId){
+		System.out.print(testId + ": ");
+		if (expectedResult){
+			if (a == b) System.out.println("passed.");
+			else System.out.println("FAILED. Pointers do not reference the same object");
+		} else {
+			if (a != b) System.out.println("passed.");
+			else System.out.println("FAILED. Pointers reference the same object");
+		}
+	}
 }
