556fe13e0a13e2b9c26274f81a41044f67f8e5b7
Caleb <caleb@whataloadofrubbish.com>
2014-12-20 13:00:29 -0600

    init commit stackImpl class, empty methods only

---
+++ StackImpl.java
@@ -0,0 +1,38 @@
+
+public class StackImpl extends AbstractStack{
+
+	public StackImpl(List list) {
+		super(list);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void push(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ReturnObject top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
