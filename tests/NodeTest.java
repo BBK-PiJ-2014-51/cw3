04e29249a184e930d60b8da95713ea38bdd69ad9
Caleb Clayton <caleb@iMac.home>
2015-01-25 16:25:07 +0000

    refactored node test into junit test

--- tests/NodeTest.java
+++ tests/NodeTest.java
@@ -1,17 +1,29 @@
 package tests;
 import node.Node;
 import node.NodeImpl;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 
-public class NodeTest extends Test{
-	public static void main(String[] args) {
-		Node node1 = new NodeImpl();
-		Node node2 = new NodeImpl(new Integer(10));
-		NodeTest test = new NodeTest();
-		
+public class NodeTest{
+	Node node1;
+	
+	@Before
+	public void build(){
+		node1 = new NodeImpl();
+	}
+	
+	@Test
+	public void getValue(){
 		node1.setValue(new Integer(5));
-		test.testForExpectedValue(node1.getValue(), new Integer(5), "Test 01: set and then get node value");
+		assertEquals(new Integer(5), node1.getValue());
+	}
+	
+	@Test
+	public void getNext(){
+		Node node2 = new NodeImpl(new Integer(10));
 		node1.setNext(node2);
-		test.testForExpectedValue(node1.getNext(), node2, "Test 02: set and then get next node");
+		assertEquals(node2, node1.getNext());
 	}
 }
