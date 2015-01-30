b3de88168ccbcc3ec80b3786d8e015183ad45424
Caleb <caleb@whataloadofrubbish.com>
2015-01-30 12:17:45 +0000

    added comments to return object impl

--- returnobject/ReturnObjectImpl.java
+++ returnobject/ReturnObjectImpl.java
@@ -5,8 +5,10 @@ public class ReturnObjectImpl implements ReturnObject{
 	private ErrorMessage errMsg;
 	private Object returnValue;
 	
-	/*
+	/**
 	 * Constructor used to wrap error messages
+	 * 
+	 * @param ErrorMsg to set initially
 	 */
 	public ReturnObjectImpl(ErrorMessage msg){
 		hasError = !(msg == ErrorMessage.NO_ERROR);
@@ -14,8 +16,10 @@ public class ReturnObjectImpl implements ReturnObject{
 		returnValue = null;
 	}
 	
-	/*
+	/**
 	 * Constructor used to wrap return values
+	 * 
+	 * @param value to set initially
 	 */
 	public ReturnObjectImpl(Object value){
 		hasError = false;
@@ -23,16 +27,28 @@ public class ReturnObjectImpl implements ReturnObject{
 		returnValue = value;
 	}
 	
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public boolean hasError() {
 		return hasError;
 	}
-
+	
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public ErrorMessage getError() {
 		return errMsg;
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public Object getReturnValue() {
 		return returnValue;
