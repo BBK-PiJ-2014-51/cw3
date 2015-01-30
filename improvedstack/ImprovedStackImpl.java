ac65e0cf7ac64626fcde770a74cccdade131e43b
Caleb <caleb@whataloadofrubbish.com>
2015-01-30 12:01:30 +0000

    added comments to improved stack class

--- improvedstack/ImprovedStackImpl.java
+++ improvedstack/ImprovedStackImpl.java
@@ -8,31 +8,57 @@ import stack.StackImpl;
 public class ImprovedStackImpl implements ImprovedStack{
 	Stack stack = new StackImpl(new LinkedList());
 	
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public boolean isEmpty() {
 		return stack.isEmpty();
 	}
-
+	
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public int size() {
 		return stack.size();
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public void push(Object item) {
 		stack.push(item);
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public ReturnObject top() {
 		return stack.top();
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 */
 	@Override
 	public ReturnObject pop() {
 		return stack.pop();
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 * Stack is completely popped to create reversed stack. A copy is kept and
+	 * the original stack is the re-loaded.
+	 */
 	@Override
 	public ImprovedStack reverse() {
 		ImprovedStack reversedStack = new ImprovedStackImpl();
@@ -51,7 +77,11 @@ public class ImprovedStackImpl implements ImprovedStack{
 		
 		return reversedStack;
 	}
-
+	
+	/**
+	 * @inheritDoc
+	 *  
+	 */
 	@Override
 	public void remove(Object object) {
 		ImprovedStack newStack = new ImprovedStackImpl();
