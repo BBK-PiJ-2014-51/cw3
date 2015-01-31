0d2f01a79dff484be5c444b6bb21e5e5de1cdc9e
Caleb Clayton <caleb@iMac.home>
2015-01-31 17:02:32 +0000

    Refacted test into junit test

--- tests/ImprovedStackTest.java
+++ tests/ImprovedStackTest.java
@@ -1,9 +1,69 @@
 package tests;
+import java.util.Arrays;
+import java.util.Collection;
+
+import list.ArrayList;
+import list.LinkedList;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import stack.Stack;
+import stack.StackImpl;
 import improvedstack.ImprovedStack;
 import improvedstack.ImprovedStackImpl;
 
-
-public class ImprovedStackTest extends Test {
+@RunWith(Parameterized.class)
+public class ImprovedStackTest{
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] { { ARRAYLIST_ID }, { LINKEDLIST_ID } };
+		return Arrays.asList(data);
+	}
+	
+	private ImprovedStack stack;
+	private static final int INIT_SIZE = 12;
+	private static final int ARRAYLIST_ID = 0;
+	private static final int LINKEDLIST_ID = 1;
+	private int currentListType;
+	
+	public ImprovedStackTest(int listType){
+		if (listType == ARRAYLIST_ID) stack = new ImprovedStackImpl(new ArrayList());
+		else if (listType == LINKEDLIST_ID) stack = new ImprovedStackImpl(new LinkedList());
+		this.currentListType = listType;
+	}
+	
+	@Test
+	public void reverse(){
+		ImprovedStack reversedStack = new ImprovedStackImpl();
+		for (int i = 0; i < INIT_SIZE; i++){
+			stack.push(new Integer(i));
+		}
+		for (int i = INIT_SIZE -1; i >= 0; i--){
+			reversedStack.push(new Integer(i));
+		}
+		
+		boolean allEqual = true;
+		for (int i = 0; i < INIT_SIZE; i++){
+			if(!stack.pop().getReturnValue().equals(reversedStack.pop().getReturnValue())) allEqual = false;
+		}
+		assertEquals(true, allEqual);
+	}
+	
+	@Test 
+	public void remove(){
+		for (int i = 0; i <= INIT_SIZE; i++){
+			stack.push(new Integer((i % 2 == 0) ? 3 : i));
+			
+		}
+		stack.remove(new Integer(3));
+		assertEquals(5, stack.size());
+	}
+	/* old tests
+	 
 	public static void main(String[] args) {
 		ImprovedStackTest test = new ImprovedStackTest();
 		ImprovedStack stack = new ImprovedStackImpl();
@@ -24,4 +84,5 @@ public class ImprovedStackTest extends Test {
 		stack.remove(new Integer(3));
 		test.testForExpectedValue(stack.size(), 5, "Test 02: checking size after removing specified elements");				
 	}
+	*/
 }
